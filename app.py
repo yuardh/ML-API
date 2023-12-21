accba5eda807865e29e6603bec0e6a52504305f3
Yuditia <ardyudit@gmail.com>
2023-12-21 12:01:23 +0700

    fix: app.py

--- app.py
+++ app.py
@@ -224,4 +224,4 @@ def prediction_route():
         }), 405
 
 if __name__ == "__main__":
-    app.run(debug = False, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+    app.run(debug = True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
